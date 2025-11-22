452ad0e9672e6e39a0741fd202efd65adbc8a167
Deon Robinson <deon@appmunki.com>
2025-11-21 16:00:12 -0800

    fix lint issues

--- packages/trpc/src/trpc.ts
+++ packages/trpc/src/trpc.ts
@@ -8,3 +8,7 @@ export const t = initTRPC.context<Context>().create({
 
 export const router = t.router;
 export const procedure = t.procedure;
+
+// Aliases for backwards compatibility
+export const createTRPCRouter = t.router;
+export const privateProcedure = t.procedure;
