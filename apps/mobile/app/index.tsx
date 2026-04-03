70e186027edb2d853b8576cc22a283171ac39c1a
Deon Robinson <deon@appmunki.com>
2026-04-02 19:07:47 -0700

    display env variables

--- apps/mobile/app/index.tsx
+++ apps/mobile/app/index.tsx
@@ -1,5 +1,11 @@
 import OnboardingFlow from '@/components/onboarding-flow';
 
+
+//display env variables
 export default function OnboardingScreen() {
+  console.log("EXPO_PUBLIC_API_BASE_URL:", process.env.EXPO_PUBLIC_API_BASE_URL);
+  console.log("EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY:", process.env.EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY);
+  console.log("EXPO_PUBLIC_COOP_ID:", process.env.EXPO_PUBLIC_COOP_ID);
+  
   return <OnboardingFlow />;
 }
